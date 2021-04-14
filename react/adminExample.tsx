eaf44afd0dcbe777e45326dbf124ffb36b037c46
mcrispo <m.crispo@arteficegroup.com>
2021-04-14 11:19:59 +0200

    VTEX

---
+++ react/adminExample.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import { Layout, PageBlock } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
+import helloworld from './graphql/helloworld.gql'
+
+const AdminExample: FC = () => {
+const { data } = useQuery(helloworld)
+
+  return (
+    <Layout>
+      <PageBlock title="Title" subtitle="Some explanation." variation="full">
+        <h1>Hello, World!</h1>
+        <p>{data?.helloworld}</p>
+      </PageBlock>
+    </Layout>
+  )
+}
+
+export default AdminExample
